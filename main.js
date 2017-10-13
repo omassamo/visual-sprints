28a137b759617536dd62522158ddaa56664e3dec
Matias Bruhn Klenz <samodenmark@gmail.com>
2017-10-13 23:37:37 +0200

    Get all cards + disable button after click
    When I have input my airtable key and link to base and click get cards, i will get the cards from the airtable base.
    
    As a user I want the get cards button to be disabled after 1 click so I don't have the option to add all cards again

--- main.js
+++ main.js
@@ -1,43 +1,101 @@
 $(document).ready(function() {
-// Request (GET https://api.airtable.com/v0/appjKgplDb7dwCcjw/Table%201)
 
-jQuery.ajax({
-    url: "https://api.airtable.com/v0/appjKgplDb7dwCcjw/Table%201",
-    type: "GET",
-    data: {
-        "api_key": "keycGQqA0T3nBwQay",
-    },
-    headers: {
-        "Cookie": "AWSALB=A1wexWjIQLVw5QWo5kKTUNY7yd1vt4E3VnGLvwidH+WcRfYPrA8qklJhNBMvV/TLq0JzPV24tjtH2na6QDlxxuDRIJL3j3vHjysDsGKvwV5TCMhYfvkjr1ugCDxw",
-    },
-})
-.done(function(data, textStatus, jqXHR) {
-    console.log("HTTP Request Succeeded: " + jqXHR.status);
-    console.log(data);
+	$( '#key' ).click(function() {
+    var link = $( '#baseLink' ).val();
+    var key = $( '#apiKey' ).val();
+    console.log(link + key); 
+    $("#key").attr("disabled", true);
 
-// jQuery each loop
+    // Fetch data from airtable 
+	   	
+	   	jQuery.ajax({
+	       url: link, //https://api.airtable.com/v0/appjKgplDb7dwCcjw/Table%201
+	       type: "GET", 
+	       data: {
+	           "api_key": key, //keycGQqA0T3nBwQay
+	       },
+	   		/*   headers: {
+	           "Cookie": "AWSALB=A1wexWjIQLVw5QWo5kKTUNY7yd1vt4E3VnGLvwidH+WcRfYPrA8qklJhNBMvV/TLq0JzPV24tjtH2na6QDlxxuDRIJL3j3vHjysDsGKvwV5TCMhYfvkjr1ugCDxw",
+	       }, */
+	   	})
+	   	.done(function(data, textStatus, jqXHR) {
+	       console.log("HTTP Request Succeeded: " + jqXHR.status);
+	       console.log(data);
 
-    $.each( data.records, function(i, val){
-      console.log(data.records[i].fields.name);
-      console.log(data.records[i].id);
-      $("#records").append(
-        "<div class='story' style='width: " + data.records[i].fields.points + "%;'><div> Story: " 
-        + data.records[i].fields.story + 
-        "</div><div>Points: " 
-        + data.records[i].fields.points + 
-        "</div><div>Description: " 
-        + data.records[i].fields.description +
-        "</div></div>"
-        );
+	   		// jQuery each loop
 
-    })
+	       	$.each( data.records, function(i, val){
+	         console.log(data.records[i].fields.name);
+	         console.log(data.records[i].id);
+	         $("#records").append(
+	           "<div class='story' style='width: " + data.records[i].fields.points + "%;'><div> Story: " 
+	           + data.records[i].fields.story + 
+	           "</div><div>Points: " 
+	           + data.records[i].fields.points + 
+	           "</div><div>Description: " 
+	           + data.records[i].fields.description +
+	           "</div></div>"
+	           );
 
-})
-.fail(function(jqXHR, textStatus, errorThrown) {
-    console.log("HTTP Request Failed");
-})
-.always(function() {
-    /* ... */
+	       	})
+
+	   })
+	   .fail(function(jqXHR, textStatus, errorThrown) {
+	       console.log("HTTP Request Failed");
+	   })
+	   .always(function() {
+	       /* ... */
+	   });
+	});
 });
-});
+
+
+// !!!!!!!!Get test cards immediately !!!!!!!!
+
+// $(document).ready(function() {
+
+// // Request (GET https://api.airtable.com/v0/appjKgplDb7dwCcjw/Table%201)
+
+// jQuery.ajax({
+//     url: "https://api.airtable.com/v0/appjKgplDb7dwCcjw/Table%201",
+//     type: "GET",
+//     data: {
+//         "api_key": "keycGQqA0T3nBwQay",
+//     },
+//     headers: {
+//         "Cookie": "AWSALB=RnDwuiauTbdEezuoNCHz0iZpiBkSxaLh/adOdKXUHXLYuHNjUXYKFLbfga3oQ384lWkr/v5XwLcGggPY9cVNv8BGcy9ZACI6ntKNrUss6kJv+aUpWREZXDBRYoek",
+//     },
+// })
+// .done(function(data, textStatus, jqXHR) {
+//     console.log("HTTP Request Succeeded: " + jqXHR.status);
+//     console.log(data);
+
+//     // jQuery each loop
+
+// 	       	$.each( data.records, function(i, val){
+// 	         console.log(data.records[i].fields.name);
+// 	         console.log(data.records[i].id);
+// 	         $("#records").append(
+// 	           "<div class='story' style='width: " + data.records[i].fields.points + "%;'><div> Story: " 
+// 	           + data.records[i].fields.story + 
+// 	           "</div><div>Points: " 
+// 	           + data.records[i].fields.points + 
+// 	           "</div><div>Description: " 
+// 	           + data.records[i].fields.description +
+// 	           "</div></div>"
+// 	           );
+
+// 	       	})
+// })
+// .fail(function(jqXHR, textStatus, errorThrown) {
+//     console.log("HTTP Request Failed");
+// })
+// .always(function() {
+//     /* ... */
+// });
+// });
+
+
+
+
 
