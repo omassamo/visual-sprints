bc4739c50c003ebe4c2c6436d2ae85114d2e69de
Matias Bruhn Klenz <samodenmark@gmail.com>
2018-02-02 23:45:26 +0100

    Styling + onboarding message
    - Added styling of top section.
    - Added a short onboarding message

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+	// Tooltip for link
+
 	$( '#baseLink' ).focus(function(){
 		console.log("FOCUS");
 		$('#baseLink').tooltip('show');
@@ -8,103 +10,46 @@ $(document).ready(function() {
 	//Passes values from inputs to vars
 
 	$( '#key' ).click(function() {
-	    var link = $( '#baseLink' ).val();
-	    var key = $( '#apiKey' ).val();
-	    var vel = $( '#Velocity' ).val();
-	    console.log(link + key); 
-	    $("#key").attr("disabled", true);
+	   var link = $( '#baseLink' ).val();
+	   var key = $( '#apiKey' ).val();
+	   var vel = $( '#Velocity' ).val();
+	   console.log(link + key + vel); 
+	   $("#key").attr("disabled", true);
 
-    // Fetch data from airtable 
+	// hide onboarding
+	   $("#onb").hide(400);
+
+   // Fetch data from airtable 
 	   	
-	   	jQuery.ajax({
-	       url: link, //https://api.airtable.com/v0/appjKgplDb7dwCcjw/Table%201
-	       type: "GET", 
-	       data: {
-	           "api_key": key, //keycGQqA0T3nBwQay
-	       },
-	   		  //  headers: {
-	       //     "Cookie": "AWSALB=A1wexWjIQLVw5QWo5kKTUNY7yd1vt4E3VnGLvwidH+WcRfYPrA8qklJhNBMvV/TLq0JzPV24tjtH2na6QDlxxuDRIJL3j3vHjysDsGKvwV5TCMhYfvkjr1ugCDxw",
-	       // }, 
-	   	})
-	   	.done(function(data, textStatus, jqXHR) {
-	       console.log("HTTP Request Succeeded: " + jqXHR.status);
-	       console.log(data);
+	jQuery.ajax({
+   	url: link, 
+   	type: "GET", 
+   	data: {
+      	"api_key": key, 
+    	},
 
-	   		// jQuery each loop
+	})
 
-	       	$.each( data.records, function(i, val){
-	         console.log(data.records[i].fields.name);
-	         console.log(data.records[i].id);
-	         $("#records").append(
-	           "<div class='story' style='width: " + (data.records[i].fields.points/vel)*100 + "%;'><div>" 
-	           + data.records[i].fields.story + 
-	           "</div><div>Points: " 
-	           + data.records[i].fields.points  
-	           // + "</div><div>Description: " 
-	           // + data.records[i].fields.description +
-	           // "</div></div>"
-	           );
+	.done(function(data, textStatus, jqXHR) {
+    console.log("HTTP Request Succeeded: " + jqXHR.status); 
 
-	       	})
+	// Append content for each card and set width based on story points
 
-	   })
+ 	$.each( data.records, function(i, val){
+  	console.log(data.records[i].fields.name);
+  	console.log(data.records[i].id);
+  	$("#records").append(
+     	"<div class='story' style='width: " + (data.records[i].fields.points/vel)*100 + "%;'><div>" + data.records[i].fields.story + "</div><div>Points: " + data.records[i].fields.points
+     	);
+
+ 		});
+
+	})
 	   .fail(function(jqXHR, textStatus, errorThrown) {
 	       console.log("HTTP Request Failed");
-	       "ERROR"
 	   })
 	   .always(function() {
 	       /* ... */
 	   });
 	});
 });
-
-
-// !!!!!!!!Get test cards immediately !!!!!!!!
-
-// $(document).ready(function() {
-
-// // Request (GET https://api.airtable.com/v0/appjKgplDb7dwCcjw/Table%201)
-
-// jQuery.ajax({
-//     url: "https://api.airtable.com/v0/appjKgplDb7dwCcjw/Table%201",
-//     type: "GET",
-//     data: {
-//         "api_key": "keycGQqA0T3nBwQay",
-//     },
-//     headers: {
-//         "Cookie": "AWSALB=RnDwuiauTbdEezuoNCHz0iZpiBkSxaLh/adOdKXUHXLYuHNjUXYKFLbfga3oQ384lWkr/v5XwLcGggPY9cVNv8BGcy9ZACI6ntKNrUss6kJv+aUpWREZXDBRYoek",
-//     },
-// })
-// .done(function(data, textStatus, jqXHR) {
-//     console.log("HTTP Request Succeeded: " + jqXHR.status);
-//     console.log(data);
-
-//     // jQuery each loop
-
-// 	       	$.each( data.records, function(i, val){
-// 	         console.log(data.records[i].fields.name);
-// 	         console.log(data.records[i].id);
-// 	         $("#records").append(
-// 	           "<div class='story' style='width: " + data.records[i].fields.points + "%;'><div> Story: " 
-// 	           + data.records[i].fields.story + 
-// 	           "</div><div>Points: " 
-// 	           + data.records[i].fields.points + 
-// 	           "</div><div>Description: " 
-// 	           + data.records[i].fields.description +
-// 	           "</div></div>"
-// 	           );
-
-// 	       	})
-// })
-// .fail(function(jqXHR, textStatus, errorThrown) {
-//     console.log("HTTP Request Failed");
-// })
-// .always(function() {
-//     /* ... */
-// });
-// });
-
-
-
-
-
