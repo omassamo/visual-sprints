f20f195b5a555d215def3816b4685952b297b553
Matias Bruhn Klenz <samodenmark@gmail.com>
2018-02-04 19:43:39 +0100

    Read story description
    Plug and play bootstrap modal to read story descriptions

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 		$('#baseLink').tooltip('show');
 	});
 
-	//Passes values from inputs to vars
+	//on click Passes values from inputs to vars
 
 	$( '#key' ).click(function() {
 	   var link = $( '#baseLink' ).val();
@@ -16,10 +16,10 @@ $(document).ready(function() {
 	   console.log(link + key + vel); 
 	   $("#key").attr("disabled", true);
 
-	// hide onboarding
+		// hide onboarding
 	   $("#onb").hide(400);
 
-   // Fetch data from airtable 
+   	// Fetch data from airtable 
 	   	
 	jQuery.ajax({
    	url: link, 
@@ -33,16 +33,28 @@ $(document).ready(function() {
 	.done(function(data, textStatus, jqXHR) {
     console.log("HTTP Request Succeeded: " + jqXHR.status); 
 
-	// Append content for each card and set width based on story points
+
+		// Append content for each card and set width based on story points
 
  	$.each( data.records, function(i, val){
-  	console.log(data.records[i].fields.name);
-  	console.log(data.records[i].id);
-  	$("#records").append(
-     	"<div class='story' style='width: " + (data.records[i].fields.points/vel)*100 + "%;'><div>" + data.records[i].fields.story + "</div><div>Points: " + data.records[i].fields.points
-     	);
+  		console.log(data.records[i]);
+  		$("#records").append(
+     		"<div class='story' style='width: " + (data.records[i].fields.points/vel)*100 + "%;'><div>" + data.records[i].fields.story + "</div><div>Points: " + data.records[i].fields.points + "</br></br><a data-toggle='modal' data-id='" + data.records[i].id + "' class='storyDescription' href='#'>👀 read<div class='descr hide'>" + data.records[i].fields.description + "</div></a>"
+     		);
 
  		});
+
+ 			// Open modal and show the hidden description element
+
+	(function($) {
+	    var infoModal = $('#myModal');
+	    $('.storyDescription').on('click', function(){
+	        htmlData = $(this).find('.descr').html();
+	        infoModal.find('.modal-body').html(htmlData);
+	        infoModal.modal('show');
+	        return false;
+	    });
+	})(jQuery); 			
 
 	})
 	   .fail(function(jqXHR, textStatus, errorThrown) {
